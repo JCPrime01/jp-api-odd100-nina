a138502452a6799a4c984b9e9c15b6bff6b5db49
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-15 20:04:00 -0300

    chore: update shareCode to Z0O60LNNLFX

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=8GIBTMNIVML&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=Z0O60LNNLFX&afp=tapi100' });
     res.end();
   }
