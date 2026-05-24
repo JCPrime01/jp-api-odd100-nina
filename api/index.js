10141f249ec7bcde8a17472bf262d7c18c34d231
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-24 19:28:02 -0300

    chore: update shareCode to Y1WT1QCRWAD

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=ZWKWCJL0SFW&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=Y1WT1QCRWAD&afp=tapi100' });
     res.end();
   }
