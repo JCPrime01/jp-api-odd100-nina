2240b0c559221e4076ed9340805bf7ce55d8bff9
JCPrime01 <vanzokiller@gmail.com>
2026-06-01 22:37:51 -0300

    chore: shareCode RJ87STUPBOP -> 1PCJZCL2RG0 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=RJ87STUPBOP&afp=tp' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/ezfr0kze?shareCode=1PCJZCL2RG0&afp=tp' });
     res.end();
   }
